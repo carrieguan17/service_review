7338372298829a7e4c9cc56ad8e5b8896909dd4d
Carrie Guan <carrieguan17@gmail.com>
2020-09-09 20:28:27 -0700

    host changed

--- database/postgres/index.js
+++ database/postgres/index.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg')
 const client = new Client({
   user: 'ec2-user',
-  host: 'http://13.52.163.55',
+  host: 'http://13.52.163.55/2250',
   database: 'review',
   port: 5432
 })
